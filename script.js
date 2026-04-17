bc4a61bd189efd642056ff4a072f5c39ce1835b3
fahrilmi25 <fahrilmi25@gmail.com>
2026-04-17 09:41:51 +0700

    feat: add handling error

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 'use strict';
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
+
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flags.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(data.population / 10000000).toFixed(1)} People</p>
+      <p class="country__row"><span>🗣️</span>${Object.values(data.languages)[0]}</p>
+      <p class="country__row"><span>💰</span>${Object.keys(data.currencies)[0]}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  // countriesContainer.style.opacity = 1;
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  // countriesContainer.style.opacity = 1;
+};
 //////////////////////////////////////////////
 // FIRST AJAX XMLHttpRequest
 /*
@@ -136,27 +158,13 @@ setTimeout(() => {
 
 //////////////////////////////////////////////
 // PROMISES
-const renderCountry = function (data, className = '') {
-  const html = `
-  <article class="country ${className}">
-    <img class="country__img" src="${data.flags.png}" />
-    <div class="country__data">
-      <h3 class="country__name">${data.name.common}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(data.population / 10000000).toFixed(1)} People</p>
-      <p class="country__row"><span>🗣️</span>${Object.values(data.languages)[0]}</p>
-      <p class="country__row"><span>💰</span>${Object.keys(data.currencies)[0]}</p>
-    </div>
-  </article>
-  `;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
-
 const getCountryData = function (country) {
   // promise
   fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then(response => response.json())
+    .then(
+      response => response.json(),
+      // err => alert(err),
+    )
     .then(data => {
       renderCountry(data[0]);
 
@@ -165,7 +173,32 @@ const getCountryData = function (country) {
 
       return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
     })
-    .then(response => response.json())
-    .then(data => renderCountry(data[0], 'neighbour'));
+    .then(
+      response => response.json(),
+      // err => alert(err),
+    )
+    .then(data => {
+      renderCountry(data[0], 'neighbour');
+
+      const neighbour2 = data[0].borders?.[1];
+      if (!neighbour2) return;
+
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour2}`).then(
+        response =>
+          response.json().then(data => renderCountry(data[0], 'neighbour')),
+      );
+    })
+    .catch(err => {
+      console.log(`${err} 💥💥💥`);
+      renderError(`Something went wrong 💥💥💥 ${err.message}. Try again!!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
 };
-getCountryData('indonesia');
+
+btn.addEventListener('click', e => {
+  getCountryData('portugal');
+});
+
+// getCountryData('bjirlah');
